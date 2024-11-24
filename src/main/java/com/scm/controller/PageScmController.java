7e516328cb11822e5ac94b875a00b7902bfd4062
Suryawanshi Sangramsingh Sambhaji <suryawanshisangramsinh@gmail.com>
2024-11-24 22:22:15 +0530

    smart-contact-manager

---
+++ src/main/java/com/scm/controller/PageScmController.java
@@ -0,0 +1,16 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageScmController 
+{
+	@RequestMapping("/home")
+	public String home(Model model)
+	{
+		model.addAttribute("name", "Sangramsingh");
+		return "home";
+	}
+}
