8f5550c6c4c5b7dfd6b429bf9f33d458f1f33169
Suryawanshi Sangramsingh Sambhaji <suryawanshisangramsinh@gmail.com>
2024-11-25 00:38:47 +0530

    updated

--- src/main/java/com/scm/controller/PageScmController.java
+++ src/main/java/com/scm/controller/PageScmController.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class PageScmController 
 {
 	@RequestMapping("/home")
-	public String home(Model model)
-	{
+	public String home(Model model){
 		model.addAttribute("name", "Sangramsingh");
 		return "home";
 	}
+	@RequestMapping("/about")
+	public String aboutPage() {
+		return "about";
+	}
+	@RequestMapping("/service")
+	public String servicePage() {
+		return "service";
+	}
+	
 }
